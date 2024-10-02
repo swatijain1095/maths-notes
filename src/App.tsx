0b4a56a15d3a140d992dff6f68cb706d719d1062
Swati Jain <jain.swati71@gmail.com>
2024-10-02 20:24:12 +0200

    added themetoggler and dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import MathCanvas from "./components/MathCanvas";
+import { ThemeProvider } from "./components/ThemeProvider";
+import { ThemeToggle } from "./components/ThemeToggle";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="flex justify-end">
+        <ThemeToggle />
+      </div>
       <MathCanvas />
-    </>
+    </ThemeProvider>
   );
 }
 
