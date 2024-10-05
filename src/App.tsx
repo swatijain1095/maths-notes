1082b0d70124ecc3b822af493361088fda4b2fb5
Swati Jain <jain.swati71@gmail.com>
2024-10-05 19:07:23 +0200

    removed div from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import "./App.css";
 import MathCanvas from "./components/MathCanvas";
 import { ThemeProvider } from "./components/ThemeProvider";
-import { ThemeToggle } from "./components/ThemeToggle";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div className="p-4">
-        <div className="flex justify-between items-center mb-4">
-          <h1
-            className="text-4xl font-bold text-black dark:text-white"
-            style={{ zIndex: 100 }}
-          >
-            Maths Notes
-          </h1>
-          <ThemeToggle />
-        </div>
-        <MathCanvas />
-      </div>
+      <MathCanvas />
     </ThemeProvider>
   );
 }
